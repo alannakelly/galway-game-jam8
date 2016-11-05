158404bfe8a28d74557f421119b6273ce1f3d41d
Mark <mark@bhalash.com>
2016-11-05 19:37:03 +0000

    Continued work changing code to be nicely-sandboxed.

--- js/flow.js
+++ js/flow.js
@@ -1,46 +1,34 @@
-define('flow', ['pixi'], function(PIXI) {
-    var colours = [
-        0xFF7F7F, 0xFFC07F, 0xFFFF7F, 0xC0FF7F,
-        0x7FFF7F, 0x7FFFC0, 0x7FFFFF, 0x7FC0FF,
-        0x7F7FFF, 0xC07FFF, 0xFF7FFF, 0xFF7FC0
-    ];
-
-    function resetParticle(particle, renderer) {
-        particle.anchor.set(0.5);
-        particle.x = renderer.width / 2.0;
-        particle.y = renderer.height / 2.0;
-        particle.direction = Math.random() * Math.PI / 16;
-        particle.turningSpeed = 0.0;
-        particle.speed = (2 * Math.random()) + 0.25;
-        particle.tint = colours[ Math.floor(Math.random() * colours.length)];
-        particle.alpha = 0.7;
+define('flow', ['pixi', 'particle'], function(PIXI, Particle) {
+    function Flow(particleCount, stage, renderer) {
+        this.container = new PIXI.Container();
+        this.bounds = new PIXI.Rectangle(-32, -32, renderer.width + 32, renderer.height + 32);
+        this.renderer = renderer;
+        this.addParticles(particleCount);
+        stage.addChild(this.container);
     }
 
-    return function(particleCount, stage, renderer) {
-        var particles = [],
-            container = new PIXI.Container(),
-            bounds = new PIXI.Rectangle(-32, -32, renderer.width + 32, renderer.height + 32);
+    Flow.prototype.particles = [];
 
-        for (var i = 0; i < particleCount; i++) {
-            var particle = PIXI.Sprite.fromImage('assets/particle.png');
-            resetParticle(particle, renderer);
-            particles.push(particle);
-            container.addChild(particle);
-        }
+    Flow.prototype.addParticles = function(count) {
+        const particle = new Particle('assets/particle.png');
 
-        stage.addChild(container);
+        particle.reset(this.renderer);
+        this.particles.push(particle);
+        this.container.addChild(particle);
 
-        return {
-            update: () => {
-                particles.forEach(particle => {
-                    if (bounds.contains(particle.x, particle.y)) {
-                        particle.x += Math.cos(particle.direction) * particle.speed;
-                        particle.y += Math.sin(particle.direction) * particle.speed;
-                    } else {
-                        resetParticle(particle);
-                    }
-                });
-            }
-        };
+        if (count) this.addParticles(count - 1);
     };
+
+    Flow.prototype.update = function() {
+        this.particles.forEach(particle => {
+            if (this.bounds.contains(particle.x, particle.y)) {
+                particle.x += Math.cos(particle.direction) * particle.speed;
+                particle.y += Math.sin(particle.direction) * particle.speed;
+            } else {
+                particle.reset(this.renderer);
+            }
+        });
+    };
+
+    return Flow;
 });
--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-define(['pixi', 'vector', 'flow'], function(PIXI, Vector, Flow) {
+define(['pixi', 'vector', 'flow', 'player'], function(PIXI, Vector, Flow, Player) {
     var renderer = PIXI.autoDetectRenderer(1280, 720, { antialias: true }),
+        player = new Player('awdawdawdawd', [0, 0]),
+        flow = new Flow(500, stage, renderer),
         stage = new PIXI.Container(),
-        flow = Flow(500, stage, renderer),
         tick = 0;
 
     function animate() {
         flow.update();
         renderer.render(stage);
+        player.draw(renderer);
         requestAnimationFrame(animate);
         tick += 0.1;
     }
 
     document.body.appendChild(renderer.view);
     stage.interactive = true;
-    requestAnimationFrame(animate);
+    animate();
 });
