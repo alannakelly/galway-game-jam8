7f821ee92a3dddcc18d1cfd92d3402713ea445e4
Alanna Kelly <alanna.kelly@insight-centre.org>
2016-11-05 16:19:09 +0000

    Flow Working

--- js/flow.js
+++ js/flow.js
@@ -1,28 +1,49 @@
-var Flow = (function( n ) {
+var Flow = (function( n, stage, renderer ) {
+
+	var colours = [ 
+		0xFF7F7F, 0xFFC07F, 0xFFFF7F, 0xC0FF7F,
+	    0x7FFF7F, 0x7FFFC0, 0x7FFFFF, 0x7FC0FF, 
+		0x7F7FFF, 0xC07FFF, 0xFF7FFF, 0xFF7FC0 ]
+
 	var particles = [];
-	var container = new PIXI.ParticleContainer( n, {
-   		scale: true,
-   		position: true,
-   		rotation: true,
-   		uvs: true,
-    	alpha: true
-	});
+	var container = new PIXI.Container();
+
+	var bounds = new PIXI.Rectangle( -32, -32, renderer.width + 32, renderer.height + 32 )
+
+	function resetParticle( particle ) {
+		particle.anchor.set(0.5);
+		particle.x = renderer.width / 2.0;
+		particle.y = renderer.height / 2.0;		
+		particle.direction = Math.random() * Math.PI / 16;
+		particle.turningSpeed = 0.0;
+		particle.speed = (2 * Math.random()) + 0.25;
+		particle.tint = colours[ Math.floor(Math.random() * colours.length ) ];
+		particle.alpha = 0.7;
+	}
+
+	for( var i = 0; i < n; i++ ) {
+		var particle = PIXI.Sprite.fromImage('assets/particle.png');
+		resetParticle( particle );
+		particles.push(particle)
+		container.addChild(particle);	
+	}
+
 	
 	stage.addChild(container);
 
 	return {
-		init : function() {
-			for( var i = 0; i < n; i++ ) {
-				var particle = PIXI.Sprite.fromImage('asssets/particle.png');
-				particle.setAnchor(0.5);
-				particle.x = renderer.width / 2;
-				particle.y = renderer.height / 2;
-				particle.direction = Math.random() * Math.PI * 2;
-				particle.turningSpeed = 0.0;
-				particle.speed = (5 + Math.random * 5);
-				particles.push(particle)
-			}
+		update : function() {
+			for( var i = 0; i < particles.length; i++ ) {
+				var particle = particles[ i ];
+				if( bounds.contains( particle.x, particle.y ) ) {
+					particle.x +=  Math.cos(particle.direction) * particle.speed;
+					particle.y += Math.sin(particle.direction) * particle.speed;
+				} else {
+					resetParticle( particle );
+				}				
+			} 
 		}
 	}
 	
 });
+
